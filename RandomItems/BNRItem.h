4060cf5797d6b082df408dbe859629f8cb5c33b7
nickmeinhold <nickmeinhold@gmail.com>
2014-03-22 14:30:09 +1100

    Initial Commit, for moving over to laptop.

---
+++ RandomItems/BNRItem.h
@@ -0,0 +1,31 @@
+//
+//  BNRItem.h
+//  RandomItems
+//
+//  Created by Nick Meinhold on 22/03/2014.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRItem : NSObject
+{
+    NSString *_itemName;
+    NSString *_serialNumber;
+    int _valueInDollars;
+    NSDate *_dateCreated;
+    
+}
+
+-(void)setItemName:(NSString*)str;
+-(NSString*)itemName;
+
+-(void)setSerialNumber:(NSString*)str;
+-(NSString*)serialNumber;
+
+-(void)setValueInDollars:(int)v;
+-(int)valueInDollars;
+
+-(NSDate*)dateCreated;
+
+@end
--- RandomItems/main.m
+++ RandomItems/main.m
@@ -7,14 +7,42 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BNRItem.h"
 
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // Create a mutable array object, store its address in items variable
+        NSMutableArray *items = [[NSMutableArray alloc]init];
+        // Send the message addobject
+        [items addObject:@"One"];
+        [items addObject:@"Two"];
+        [items addObject:@"Three"];
+        
+        // send another message, insertObject
+        [items insertObject:@"Zero" atIndex:0];
+        
+        // for every item in the items array
+        for(NSString *item in items)
+        {
+            NSLog(@"%@", item);
+        }
+        
+        BNRItem *item = [[BNRItem alloc]init];
+        
+//        [item setItemName:@"Red Sofa"];
+        item.itemName = @"Red Sofa";
+//        [item setSerialNumber:@"1234"];
+        item.serialNumber = @"1234";
+//        [item setValueInDollars:100];
+        item.valueInDollars = 100;
+        NSLog(@"%@ %@ %@ %d", [item itemName], [item dateCreated], [item serialNumber], [item valueInDollars]);
+        
+        
+        // destroy the mutable array
+        items = nil;
         
     }
     return 0;
